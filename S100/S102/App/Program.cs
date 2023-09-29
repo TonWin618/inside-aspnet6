44369569a8eef2bff65b348c66abe854721a6080
汤问 <tonwin618@qq.com>
2023-09-29 15:12:24 +0800

    S102

---
+++ S100/S102/App/Program.cs
@@ -0,0 +1,7 @@
+﻿//RequestDelegate的本质就是Func<HttpContext, Task>类型的委托
+RequestDelegate handler = context => context.Response.WriteAsync("Hello, World!");
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplication app = builder.Build();
+//没有指定路由路径，所有请求都通过这个处理器处理
+app.Run(handler: handler);
+app.Run();
