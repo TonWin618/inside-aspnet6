c7ab17d472d6fdd510171f02b977e0899702eac4
汤问 <tonwin618@qq.com>
2023-09-29 18:52:04 +0800

    S105, S106

---
+++ S100/S105/App/Program.cs
@@ -0,0 +1,14 @@
+﻿var app = WebApplication.Create(args);
+app
+    .Use(middleware: HelloMiddleware)
+    .Use(middleware: WorldMiddleware);
+app.Run();
+
+//Use方法内部会将Func<HttpContext, HttpRequest, Task>转化内Func<RequestDelegate, RequestDelegate>
+static async Task HelloMiddleware(HttpContext httpContext, RequestDelegate next){
+    await httpContext.Response.WriteAsync("Hello, ");
+    await next(httpContext);
+}
+
+static Task WorldMiddleware(HttpContext httpContext, RequestDelegate next)
+    => httpContext.Response.WriteAsync("World!");
---
+++ S100/S106/App/Program.cs
@@ -0,0 +1,15 @@
+﻿var app = WebApplication.Create(args);
+app
+    .Use(middleware: HelloMiddleware)
+    .Use(middleware: WorldMiddleware);
+app.Run();
+
+//Use方法内部会将Func<HttpContext, Func<Task>, Task>转化内Func<RequestDelegate, RequestDelegate>
+//
+static async Task HelloMiddleware(HttpContext httpContext, Func<Task> next){
+    await httpContext.Response.WriteAsync("Hello, ");
+    await next();
+}
+
+static Task WorldMiddleware(HttpContext httpContext, Func<Task> next)
+    => httpContext.Response.WriteAsync("World!");
