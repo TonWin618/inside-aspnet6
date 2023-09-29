0cb904f2af4b7614815e7989ca2cb1d410622c2f
汤问 <tonwin618@qq.com>
2023-09-29 18:31:11 +0800

    S104

---
+++ S100/S104/App/Program.cs
@@ -0,0 +1,23 @@
+﻿var app = WebApplication.Create(args);
+IApplicationBuilder appBuilder = app;
+appBuilder
+    .Use(middleware: HelloMiddleware)
+    .Use(middleware: WorldMiddleware);
+
+//只会输出“World!”
+//appBuilder
+//    .Use(middleware: WorldMiddleware)
+//    .Use(middleware: HelloMiddleware);
+
+app.Run();//注册处于管道末端的中间件
+
+//中间件，可体现为一个Func<RequestDelegate,RequestDelegate>的委托对象
+static RequestDelegate HelloMiddleware(RequestDelegate next) =>
+async httpContext =>
+{
+    await httpContext.Response.WriteAsync("Hello, ");
+    await next(httpContext);
+};
+
+static RequestDelegate WorldMiddleware(RequestDelegate next) =>
+    httpContext => httpContext.Response.WriteAsync("World!");
