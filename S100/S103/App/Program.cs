38b5d0eac7c098c3ce47ad460dde9557bda47746
汤问 <tonwin618@qq.com>
2023-09-29 15:19:01 +0800

    S103

---
+++ S100/S103/App/Program.cs
@@ -0,0 +1,7 @@
+﻿//快速创建 Web 应用程序，这种方式的配置选项相对较少
+var app = WebApplication.Create(args);
+app.Run(handler: HandleAsync);
+app.Run();
+//将请求处理器定义为一个静态方法
+static Task HandleAsync(HttpContext httpContext)
+    => httpContext.Response.WriteAsync("Hello, World!");
