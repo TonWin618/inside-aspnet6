4a33297d8e83f74ab3ea6c51900f6efe95401d1f
汤问 <tonwin618@qq.com>
2023-10-01 19:05:25 +0800

    S302

---
+++ S300/S302/App/Program.cs
@@ -0,0 +1,13 @@
+﻿using App;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+
+var provider = new ServiceCollection()
+    .AddTransient<IFoo, Foo>()
+    .AddTransient<IBar, Bar>()
+    .AddTransient(typeof(IFoobar<,>), typeof(Foobar<,>))
+    .BuildServiceProvider();
+
+var foobar = (Foobar<IFoo, IBar>?)provider.GetService<IFoobar<IFoo, IBar>>();
+Console.WriteLine(foobar?.Foo is Foo);
+Console.WriteLine(foobar?.Bar is Bar);
