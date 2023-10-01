ea29b60f0026b71455b3ba8ced0925bab3e772da
汤问 <tonwin618@qq.com>
2023-10-01 20:08:40 +0800

    S307

---
+++ S300/S307/App/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+//服务注册有效性校验关闭
+BuildServiceProvider(false);
+//服务注册有效性校验开启
+BuildServiceProvider(true);
+
+static void BuildServiceProvider(bool validateOnBuild)
+{
+    try
+    {
+        var options = new ServiceProviderOptions
+        {
+            ValidateOnBuild =validateOnBuild
+        };
+        new ServiceCollection()
+            .AddSingleton<IFoobar,Foobar>()
+            .BuildServiceProvider(options);
+        Console.WriteLine($"Status: Suceess; ValidateOnBuild: {validateOnBuild}");
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine($"Status: Fail; ValidateOnBuild: {validateOnBuild}");
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
+
+public interface IFoobar{}
+public class Foobar : IFoobar
+{
+    private Foobar(){}
+    //Foobar具有唯一私有构造函数，针对该务实现类的注册无效
+    public static readonly Foobar Instance = new Foobar();
+}
