a454dbeeb84871c6ea227836f3a914f59e659c85
汤问 <tonwin618@qq.com>
2023-10-01 18:54:38 +0800

    S301

---
+++ S300/S301/App/Program.cs
@@ -0,0 +1,12 @@
+﻿using App;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+
+var provider = new ServiceCollection()
+    .AddTransient<IFoo, Foo>()
+    .AddScoped<IBar>(_ => new Bar())
+    .AddSingleton<IBaz, Baz>()
+    .BuildServiceProvider();
+Console.WriteLine(provider.GetService<IFoo>() is Foo);
+Console.WriteLine(provider.GetService<IBar>() is Bar);
+Console.WriteLine(provider.GetService<IBaz>() is Baz);
