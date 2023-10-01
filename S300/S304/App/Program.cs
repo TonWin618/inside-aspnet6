b85a2788766de5bc96c98aa843e0c0b6ff3c7639
汤问 <tonwin618@qq.com>
2023-10-01 19:16:28 +0800

    S304

---
+++ S300/S304/App/Program.cs
@@ -0,0 +1,26 @@
+﻿using App;
+using Microsoft.Extensions.DependencyInjection;
+
+var root = new ServiceCollection()
+    .AddTransient<IFoo, Foo>()
+    .AddScoped<IBar>(_ => new Bar())
+    .AddSingleton<IBaz, Baz>()
+    .BuildServiceProvider();
+
+var provider1 = root.CreateScope().ServiceProvider;
+var provider2 = root.CreateScope().ServiceProvider;
+
+GetServiceTwice<IFoo>(provider1);
+GetServiceTwice<IBar>(provider1);
+GetServiceTwice<IBaz>(provider1);
+Console.WriteLine();
+GetServiceTwice<IFoo>(provider2);
+GetServiceTwice<IBar>(provider2);
+GetServiceTwice<IBaz>(provider2);
+
+
+static void GetServiceTwice<T>(IServiceProvider provider)
+{
+    provider.GetService<T>();
+    provider.GetService<T>();
+}
