74b5da06fd99a042e65e81b6c31651fc73e2d177
汤问 <tonwin618@qq.com>
2023-10-01 19:07:41 +0800

    S303

---
+++ S300/S303/App/Program.cs
@@ -0,0 +1,15 @@
+﻿using App;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+using System.Linq;
+
+var services = new ServiceCollection()
+    .AddTransient<Base, Foo>()
+    .AddTransient<Base, Bar>()
+    .AddTransient<Base, Baz>()
+    .BuildServiceProvider()
+    .GetServices<Base>();
+    
+Console.WriteLine(services.OfType<Foo>().Any());
+Console.WriteLine(services.OfType<Bar>().Any());
+Console.WriteLine(services.OfType<Baz>().Any());
