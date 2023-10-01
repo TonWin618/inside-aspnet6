287aa7af062ea16c537e9ce54e7dee9e659594bd
汤问 <tonwin618@qq.com>
2023-10-01 16:50:59 +0800

    S204

--- S200/S203/App/Program.cs
+++ S200/S203/App/Program.cs
@@ -1,10 +1,17 @@
 ﻿using App;
-var services = new Cat()
-    .Register<Base,Foo>(Lifetime.Transient)
-    .Register<Base,Bar>(Lifetime.Transient)
-    .Register<Base,Baz>(Lifetime.Transient)
-    .GetServices<Base>();
-
-Console.WriteLine(services.OfType<Foo>().Any());
-Console.WriteLine(services.OfType<Bar>().Any());
-Console.WriteLine(services.OfType<Baz>().Any());
+using (var root = new Cat()
+    .Register<IFoo, Foo>(Lifetime.Transient)
+    .Register<IBar>(_ => new Bar(), Lifetime.Self)
+    .Register<IBaz, Baz>(Lifetime.Root)
+    .Register(typeof(Foo).Assembly))//[MapTo(typeof(IQux), Lifetime.Root)] Qux
+{
+    using(var cat = root.CreateChild())
+    {
+        cat.GetService<IFoo>();
+        cat.GetService<IBar>();
+        cat.GetService<IBaz>();
+        cat.GetService<IQux>();
+        Console.WriteLine("Child cat is disposed.");
+    }
+    Console.WriteLine("Root Cat is disposed");
+}
---
+++ S200/S204/App/Program.cs
@@ -0,0 +1,10 @@
+﻿using App;
+var services = new Cat()
+    .Register<Base,Foo>(Lifetime.Transient)
+    .Register<Base,Bar>(Lifetime.Transient)
+    .Register<Base,Baz>(Lifetime.Transient)
+    .GetServices<Base>();
+
+Console.WriteLine(services.OfType<Foo>().Any());
+Console.WriteLine(services.OfType<Bar>().Any());
+Console.WriteLine(services.OfType<Baz>().Any());
