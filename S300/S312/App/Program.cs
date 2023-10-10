9b3691eca86624d6c08a4d1c2916aaeb03380426
汤问 <tonwin618@qq.com>
2023-10-10 20:58:49 +0800

    S312

---
+++ S300/S312/App/Program.cs
@@ -0,0 +1,11 @@
+﻿using App;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<Foo>()
+    .AddSingleton<Bar>()
+    .BuildServiceProvider();
+
+//var fooBar = ActivatorUtilities.CreateInstance<Foobar>(serviceProvider, "foobar");
+var fooBar = ActivatorUtilities.CreateInstance(serviceProvider, typeof(Foobar), "foobar") as Foobar;
+Console.WriteLine(fooBar?.Name);
