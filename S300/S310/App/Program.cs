20fe126a4cbb1beb34f97038ba5221e9014ab62b
汤问 <tonwin618@qq.com>
2023-10-10 20:40:41 +0800

    S310

---
+++ S300/S310/App/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using var scope = new ServiceCollection()
+    .AddScoped<Fooar>()
+    .BuildServiceProvider()
+    .CreateScope();
+
+scope.ServiceProvider.GetRequiredService<Fooar>();
+//当IServiceProvider对象的Dispose方法被执行时，释放的服务实例对应的类在实现IAsyncDisposable的同时也要实现IDisposable接口，否则报错：
+//System.InvalidOperationException: 'Fooar' type only implements IAsyncDisposable. Use DisposeAsync to dispose the container.
+public class Fooar : IAsyncDisposable
+{
+    public ValueTask DisposeAsync() => default;
+}
