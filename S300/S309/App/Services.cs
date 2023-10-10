ab7e203ef564e4128d488b5d383c603c2faeaa95
汤问 <tonwin618@qq.com>
2023-10-10 20:13:29 +0800

    S309

---
+++ S300/S309/App/Services.cs
@@ -0,0 +1,19 @@
+﻿namespace App
+{
+    public interface IFoo { }
+    public interface IBar { }
+    public interface IBaz { }
+    public interface IQux { }
+
+    public class Foo : IFoo { }
+    public class Bar : IBar { }
+    public class Baz : IBaz { }
+    public class Qux : IQux
+    {
+        //抛出InvalidOperation异常，无法从两个构造函数中选择最优的来创建服务实例。
+        public Qux(IFoo foo, IBar bar) { }
+        public Qux(IBar bar, IBaz baz) { }
+    }
+
+
+}
