8bc45785d72c420eca5075f3f7c3970f0ac70894
汤问 <tonwin618@qq.com>
2023-10-10 20:08:43 +0800

    S308

---
+++ S300/S308/App/Services.cs
@@ -0,0 +1,25 @@
+﻿namespace App
+{
+    public interface IFoo { }
+    public interface IBar { }
+    public interface IBaz { }
+    public interface IQux { }
+
+    public class Foo : IFoo { }
+    public class Bar : IBar { }
+    public class Baz : IBaz { }
+    public class Qux : IQux
+    {
+        public Qux(IFoo foo)
+            => Console.WriteLine("Selected constructor: Qux(IFoo)");
+        //最终被调用的是这个
+        //选择参数最多的合法构造函数
+        public Qux(IFoo foo, IBar bar)
+            => Console.WriteLine("Selected constructor: Qux(IFoo, IBar)");
+        //缺少参数，不合法
+        public Qux(IFoo foo, IBar bar, IBaz baz)
+            => Console.WriteLine("Selected constructor: Qux(IFoo, IBar, IBaz)");
+    }
+
+
+}
