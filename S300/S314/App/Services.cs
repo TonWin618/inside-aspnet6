d9467dece0ee7d3263e049907cc5ea3f62bfd413
汤问 <tonwin618@qq.com>
2023-10-11 20:59:05 +0800

    S314

---
+++ S300/S314/App/Services.cs
@@ -0,0 +1,20 @@
+﻿namespace App
+{
+    public class Foo { }
+    public class Bar { }
+    public class Baz { }
+
+    public class Foobar
+    {
+        //选择这个，总是第一个构造函数执行
+        public Foobar(Foo foo) => Console.WriteLine("Foobar(Foo foo)");
+        public Foobar(Foo foo, Bar bar) => Console.WriteLine("Foobar(Foo foo, Bar bar)");
+    }
+    public class BarBaz
+    {
+        //选择这个，总是第一个构造函数执行
+        public BarBaz(Bar bar, Baz baz) => Console.WriteLine("BarBaz(Bar bar, Baz baz)");
+        public BarBaz(Bar bar) => Console.WriteLine("BarBaz(Bar bar)");
+    }
+
+}
