a2b225ba34d28b9b3def872367bf49ce76045d47
汤问 <tonwin618@qq.com>
2023-10-11 22:13:29 +0800

    S403

---
+++ S400/S403/Program.cs
@@ -0,0 +1,25 @@
+﻿using App;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+var assembly = Assembly.GetEntryAssembly()!;
+
+var content = await new ServiceCollection()
+    //添加嵌入文件系统
+    .AddSingleton<IFileProvider>(new EmbeddedFileProvider(assembly))
+    .AddSingleton<IFileSystem, FileSystem>()
+    .BuildServiceProvider()
+    .GetRequiredService<IFileSystem>()
+    .ReadAllTextAsync("data.txt");
+
+//从程序集嵌入的资源中获取流，嵌入资源的名称包括命名空间
+var stream =  assembly.GetManifestResourceStream($"{assembly.GetName().Name}.data.txt");
+var buffer = new byte[stream!.Length];
+stream.Read(buffer,0,buffer.Length);
+
+Console.WriteLine(content);
+Console.WriteLine(Encoding.Default.GetString(buffer));
+
