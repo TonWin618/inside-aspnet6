f1fe4b5644fdcc4293d8913161413bacbb2123e8
汤问 <tonwin618@qq.com>
2023-10-11 21:44:13 +0800

    S401

---
+++ S400/S401/IFileSystem.cs
@@ -0,0 +1,11 @@
+﻿namespace App
+{
+    public interface IFileSystem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="print">int为层级，string为路径</param>
+        void ShowStructure(Action<int, string> print);
+    }
+}
---
+++ S400/S401/Program.cs
@@ -0,0 +1,12 @@
+﻿using App;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+
+static void Print(int layer, string name) => Console.WriteLine($"{new string(' ', layer * 4)}{name}");
+new ServiceCollection()
+    //使用物理文件系统
+    .AddSingleton<IFileProvider>(new PhysicalFileProvider(@"D:\Projects\TestProjects\InsideAspNet6\S400\S401\Test"))
+    .AddSingleton<IFileSystem, FileSystem>()
+    .BuildServiceProvider()
+    .GetRequiredService<IFileSystem>()
+    .ShowStructure(Print);
