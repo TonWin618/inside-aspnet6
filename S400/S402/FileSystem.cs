26aa195959abd2ee839fff5dd89f56d008a4a97b
汤问 <tonwin618@qq.com>
2023-10-11 21:58:15 +0800

    S402

---
+++ S400/S402/FileSystem.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System.Text;
+
+namespace App
+{
+    public class FileSystem : IFileSystem
+    {
+        private readonly IFileProvider _fileProvider;
+        public FileSystem(IFileProvider fileProvider) => _fileProvider = fileProvider;
+        public void ShowStructure(Action<int, string> print)
+        {
+            int indent = -1;
+            Print("");
+
+            void Print(string subPath)
+            {
+                //增加层级
+                indent++;
+                //返回目录内容，则遍历所有子目录和文件
+                foreach (var fileInfo in _fileProvider.GetDirectoryContents(subPath))
+                {
+                    print(indent, fileInfo.Name);
+                    //判断是否为目录
+                    if (fileInfo.IsDirectory)
+                    {
+                        Print($@"{subPath}\{fileInfo.Name}".TrimStart('\\'));
+                    }
+                }
+                indent--;
+            }
+        }
+
+        public async Task<string> ReadAllTextAsync(string path)
+        {
+            byte[] buffer;
+            //读取文件的输出流
+            using (var stream = _fileProvider.GetFileInfo(path).CreateReadStream())
+            {
+                buffer = new byte[stream.Length];
+                //将输出流写入缓存中
+                await stream.ReadAsync(buffer);
+            }
+            //将字节数组转化为字符串
+            return Encoding.Default.GetString(buffer);
+        }
+    }
+}
---
+++ S400/S402/Program.cs
@@ -0,0 +1,13 @@
+﻿using App;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using System.Diagnostics;
+
+var content = await new ServiceCollection()
+    .AddSingleton<IFileProvider>(new PhysicalFileProvider(@"D:\Projects\TestProjects\InsideAspNet6\S400\S402\Test"))
+    .AddSingleton<IFileSystem, FileSystem>()
+    .BuildServiceProvider()
+    .GetRequiredService<IFileSystem>()
+    .ReadAllTextAsync("Folder2/b.txt");
+
+Console.WriteLine(content);
