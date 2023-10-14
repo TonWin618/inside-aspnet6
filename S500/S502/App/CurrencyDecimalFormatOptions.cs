df1e272808c9ac930ae69dceca5e992fcf555472
汤问 <tonwin618@qq.com>
2023-10-14 21:12:06 +0800

    S502

---
+++ S500/S502/App/CurrencyDecimalFormatOptions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace App
+{
+    public class CurrencyDecimalFormatOptions
+    {
+        public int Digits { get; set; }
+        public string Symbol { get; set; }
+        //从配置节中读取配置
+        public CurrencyDecimalFormatOptions(IConfiguration config)
+        {
+            Digits = int.Parse(config["Digits"]);
+            Symbol = config["Symbol"];
+        }
+    }
+}
---
+++ S500/S502/App/FormatOptions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace App
+{
+    public class FormatOptions
+    {
+        public DateTimeFormatOptions DateTime { get; set; }
+        public CurrencyDecimalFormatOptions CurrencyDecimal { get; set; }
+        //从配置节中读取配置节
+        public FormatOptions(IConfiguration config)
+        {
+            DateTime = new DateTimeFormatOptions(config.GetSection("DateTime"));
+            CurrencyDecimal = new CurrencyDecimalFormatOptions(config.GetSection("CurrencyDecimal"));
+        }
+    }
+}
