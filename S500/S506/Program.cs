d70e7a1d49632d22ff6c328850fd38ea99037e82
汤问 <tonwin618@qq.com>
2023-10-15 21:14:15 +0800

    S506

---
+++ S500/S506/Program.cs
@@ -0,0 +1,30 @@
+﻿using App;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Primitives;
+
+var config = new ConfigurationBuilder()
+        //此处监听的是bin目录下的appsettings.json文件
+        .AddJsonFile("appsettings.json", true, true)
+        .Build();
+
+ChangeToken.OnChange(() => config.GetReloadToken(), () =>
+{
+        var options = config.GetSection("format").Get<FormatOptions>();
+        var dateTime = options.DateTime;
+        var currencyDecimal = options.CurrencyDecimal;
+
+        Console.WriteLine("DateTime:");
+        Console.WriteLine($"\tLongDatePattern: {dateTime.LongDatePattern}");
+        Console.WriteLine($"\tLongTimePattern: {dateTime.LongTimePattern}");
+        Console.WriteLine($"\tShortDatePattern: {dateTime.ShortDatePattern}");
+        Console.WriteLine($"\tShortTimePattern: {dateTime.ShortTimePattern}"); 
+
+        Console.WriteLine("CurrencyDecimal:");
+        Console.WriteLine($"\tDigits:{currencyDecimal.Digits}");
+        Console.WriteLine($"\tSymbol:{currencyDecimal.Symbol}");
+});
+
+Console.Read();//避免程序直接结束运行
+
+
