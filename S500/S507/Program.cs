43540627b0fc3b8655a7d3948d4e394d4daf7c39
汤问 <tonwin618@qq.com>
2023-10-21 19:00:58 +0800

    S507

---
+++ S500/S507/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+var source = new Dictionary<string, string?>
+{
+    ["foo"] = null,
+    ["bar"] = "",
+    ["baz"] = "123"
+};
+
+var root = new ConfigurationBuilder()
+    .AddInMemoryCollection(source)
+    .Build();
+
+//针对object，直接返回原始值，即字符串或者null
+Console.WriteLine(root.GetValue<object>("foo") == null);
+Console.WriteLine("".Equals(root.GetValue<object>("bar")));
+Console.WriteLine("123".Equals(root.GetValue<object>("baz")));
+
+//针对普通类型
+Console.WriteLine(root.GetValue<int>("foo") == 0);
+Console.WriteLine(root.GetValue<int>("baz") == 123);
+
+//针对Nullable<T>
+Console.WriteLine(root.GetValue<int?>("foo") == null);
+Console.WriteLine(root.GetValue<int?>("bar") == null);
