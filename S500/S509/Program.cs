95dc2e236e3e902d7e3eb28f63289615f0be6969
汤问 <tonwin618@qq.com>
2023-10-21 19:11:29 +0800

    S509

---
+++ S500/S509/Program.cs
@@ -0,0 +1,21 @@
+﻿using App;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+var source = new Dictionary<string, string>
+{
+    ["gender"] = "Male",
+    ["age"] = "18",
+    ["contactInfo:emailAddress"] = "foobar@outlook.com",
+    ["contactInfo:phoneNo"] = "123456789"
+};
+
+var configuration = new ConfigurationBuilder()
+    .AddInMemoryCollection(source)
+    .Build();
+
+var profile = configuration.Get<Profile>();
+Console.WriteLine(profile.Gender == Gender.Male);
+Console.WriteLine(profile.Age == 18);
+Console.WriteLine(profile.ContactInfo.EmailAddress == "foobar@outlook.com");
+Console.WriteLine(profile.ContactInfo.PhoneNo == "123456789");
