3386d573052b740708be685d5ec4829c9cf44648
汤问 <tonwin618@qq.com>
2023-10-21 20:04:11 +0800

    S514

---
+++ S500/S514/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+
+//需添加Microsoft.Extensions.Configuration.CommandLine的包引用
+try
+{
+    //命令行参数的全名和缩写映射
+    var mapping = new Dictionary<string, string>
+    {
+        ["-a"] = "architecture",
+        ["-arch"] = "architecture"
+    };
+    var configuration = new ConfigurationBuilder()
+        .AddCommandLine(args, mapping)
+        .Build();
+    Console.WriteLine($"Architecture: {configuration["architecture"]}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+//启动命令
+    //单参数
+        //dotnet run architecture=x64
+        //dotnet run /architecture=x64
+        //dotnet run -architecture=x64
+        //dotnet run --architecture=x64
+        //dotnet run -a=x64
+        //dotnet run -arch=x64
+    //双参数
+        //dotnet run -a x64
+        //dotnet run -arch x64
+        //dotnet run --architecture x64
+        //dotnet run /architecture x64
