2ad2c225e5f44e8dba68e1783ebea74177dd9388
汤问 <tonwin618@qq.com>
2023-10-21 19:08:07 +0800

    S508

---
+++ S500/S508/Point.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+namespace App
+{
+    [TypeConverter(typeof(PointTypeConverter))]
+    public readonly record struct Point(double X, double Y);
+
+    public class PointTypeConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
+        => sourceType == typeof(string);//支持源字符串的类型转换
+
+        public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+        {
+            var split = (value.ToString() ?? "0.0,0.0").Split(',');
+            double x = double.Parse(split[0].Trim().TrimStart('('));
+            double y = double.Parse(split[1].Trim().TrimEnd(')'));
+            return new Point(x, y);
+        }
+    }
+}
---
+++ S500/S508/Program.cs
@@ -0,0 +1,16 @@
+﻿using App;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+var source = new Dictionary<string, string>
+{
+    ["point"] = "(123,456)"
+};
+
+var root = new ConfigurationBuilder()
+    .AddInMemoryCollection(source)
+    .Build();
+
+//这里使用TypeConverter特性指定的PointTypeConverter来进行类型转换
+var point = root.GetValue<Point>("point");
+Console.WriteLine(point);
